b30abbc25eab3b79f857928439c5636a66898fd5
PondProgram <167842347+PondProgram@users.noreply.github.com>
2024-04-23 12:16:09 +0700

    Update Find_HSV.py

--- Find_HSV.py
+++ Find_HSV.py
@@ -4,31 +4,31 @@ import numpy as np
 def nothing(x):
     pass
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
-cv2.createTrackbar("Lower - Hue", "Trackbars", 0, 179, nothing)
-cv2.createTrackbar("Lower - Saturation", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("Lower - Value", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("Upper - Hue", "Trackbars", 179, 179, nothing)
-cv2.createTrackbar("Upper - Saturation", "Trackbars", 255, 255, nothing)
-cv2.createTrackbar("Upper - Value", "Trackbars", 255, 255, nothing)
+cv2.namedWindow("Trackbars")
+
+cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
 
 while True:
-   
     ret, frame = cap.read()
     if not ret:
         break
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    l_h = cv2.getTrackbarPos("Lower - Hue", "Trackbars")
-    l_s = cv2.getTrackbarPos("Lower - Saturation", "Trackbars")
-    l_v = cv2.getTrackbarPos("Lower - Value", "Trackbars")
-    u_h = cv2.getTrackbarPos("Upper - Hue", "Trackbars")
-    u_s = cv2.getTrackbarPos("Upper - Saturation", "Trackbars")
-    u_v = cv2.getTrackbarPos("Upper - Value", "Trackbars")
-    cv2.namedWindow("Trackbars")
-   
+    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+
     lower_range = np.array([l_h, l_s, l_v])
     upper_range = np.array([u_h, u_s, u_v])
 
@@ -36,11 +36,11 @@ while True:
 
     res = cv2.bitwise_and(frame, frame, mask=mask)
 
-    mask_rgb = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    mask_bgr = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
-    stacked = np.hstack((frame, mask_rgb, res))
+    stacked = np.hstack((frame, mask_bgr, res))
     resized_stacked = cv2.resize(stacked, None, fx=0.7, fy=0.7)
-    cv2.imshow('Find_HSV', resized_stacked)
+    cv2.imshow('Trackbars', resized_stacked)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
